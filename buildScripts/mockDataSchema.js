3a05cc5530928bfe7aac8015dc1f03dda7563990
Alex Demchenko <pilot@pesochin.net>
2016-12-24 16:51:30 +0200

    Add project, tasks, records

--- buildScripts/mockDataSchema.js
+++ buildScripts/mockDataSchema.js
@@ -19,18 +19,89 @@ var schema = {
           },
           "lastName": {
             "type": "string",
-            "faker": "name.lastName",
+            "faker": "name.lastName"
           },
           "email": {
             "type": "string",
-            "faker": "internet.email",
+            "faker": "internet.email"
           }
         },
-        required: ['id', 'firstName', 'lastName', 'email']
+        "required": ["id", "firstName", "lastName", "email"]
+      }
+    },
+    "projects": {
+      "type": "array",
+      "minItems": 5,
+      "items": {
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "number",
+            "unique": true,
+            "minimum": 1
+          },
+          "title": {
+            "type": "string",
+            "faker": "internet.domainName"
+          },
+          "createdAt": {
+            "type": "string",
+            "faker": "date.recent"
+          },
+          "tasks": {
+            "type": "array",
+            "minItems": 5,
+            "maxItems": 10,
+            "items": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "number",
+                  "unique": true,
+                  "minimum": 1
+                },
+                "title": {
+                  "type": "string",
+                  "faker": "hacker.phrase"
+                },
+                "createdAt": {
+                  "type": "string",
+                  "faker": "date.recent"
+                },
+                "records": {
+                  "type": "array",
+                  "minItems": 3,
+                  "maxItems": 5,
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "id": {
+                        "type": "number",
+                        "unique": true,
+                        "minimum": 1
+                      },
+                      "createdAt": {
+                        "type": "string",
+                        "faker": "date.recent"
+                      },
+                      "finishedAt": {
+                        "type": "string",
+                        "faker": "date.future"
+                      }
+                    },
+                    "required": ["id", "createdAt", "finishedAt"]
+                  }
+                }
+              },
+              "required": ["id", "title", "createdAt", "records"]
+            }
+          }
+        },
+        "required": ["id", "title", "createdAt", "tasks"]
       }
     }
   },
-  required: ['users']
+  "required": ["users", "projects"]
 };
 
 module.exports = schema;
