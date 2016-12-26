555ebb4a5637237a2b2d2bd489e78697fb286770
Alex Demchenko <pilot@pesochin.net>
2016-12-26 13:42:06 +0200

    Extend api with users / tasks / records models

--- README.md
+++ README.md
@@ -5,8 +5,15 @@ This is an API prototype for time tracker frontend application
 ```
 npm install
 npm run start-mockapi
-Load http://localhost:3001/users
+open http://localhost:3001
 ```
+
+### Available models
+
+- http://localhost:3001/users
+- http://localhost:3001/projects
+- http://localhost:3001/tasks
+- http://localhost:3001/records
 
 ### Notes
 Note that changes are saved to db.json, so they're reflected on refresh like a real database exists!
--- buildScripts/mockDataSchema.js
+++ buildScripts/mockDataSchema.js
@@ -1,40 +1,111 @@
 var schema = {
   "definitions": {
     "users": {
+      "type": "array",
+      "minItems": 3,
+      "maxItems": 5,
+      "items": {
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "number",
+            "unique": true,
+            "minimum": 1
+          },
+          "firstName": {
+            "type": "string",
+            "faker": "name.firstName"
+          },
+          "lastName": {
+            "type": "string",
+            "faker": "name.lastName"
+          },
+          "email": {
+            "type": "string",
+            "faker": "internet.email"
+          }
+        },
+        "required": ["id", "firstName", "lastName", "email"]
+      }
+    },
+    "records": {
+      "type": "array",
+      "minItems": 3,
+      "maxItems": 5,
+      "items": {
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "number",
+            "unique": true,
+            "minimum": 1
+          },
+          "createdAt": {
+            "type": "string",
+            "faker": "date.recent"
+          },
+          "finishedAt": {
+            "type": "string",
+            "faker": "date.future"
+          }
+        },
+        "required": ["id", "createdAt", "finishedAt"]
+      }
+    },
+    "user": {
       "type": "object",
       "properties": {
-        "type": "array",
-        "minItems": 3,
-        "maxItems": 5,
-        "items": {
-          "type": "object",
-          "properties": {
-            "id": {
-              "type": "number",
-              "unique": true,
-              "minimum": 1
-            },
-            "firstName": {
-              "type": "string",
-              "faker": "name.firstName"
-            },
-            "lastName": {
-              "type": "string",
-              "faker": "name.lastName"
-            },
-            "email": {
-              "type": "string",
-              "faker": "internet.email"
-            }
-          },
-          "required": ["id", "firstName", "lastName", "email"]
+        "id": {
+          "type": "number",
+          "unique": true,
+          "minimum": 1
+        },
+        "firstName": {
+          "type": "string",
+          "faker": "name.firstName"
+        },
+        "lastName": {
+          "type": "string",
+          "faker": "name.lastName"
+        },
+        "email": {
+          "type": "string",
+          "faker": "internet.email"
         }
+      },
+      "required": ["id", "firstName", "lastName", "email"]
+    },
+    "tasks": {
+      "type": "array",
+      "minItems": 5,
+      "maxItems": 10,
+      "items": {
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "number",
+            "unique": true,
+            "minimum": 1
+          },
+          "title": {
+            "type": "string",
+            "faker": "hacker.phrase"
+          },
+          "user": { "$ref": "#/definitions/user" },
+          "createdAt": {
+            "type": "string",
+            "faker": "date.recent"
+          },
+          "records": { "$ref": "#/definitions/records" }
+        },
+        "required": ["id", "title", "user", "createdAt", "records"]
       }
     }
   },
   "type": "object",
 
   "properties": {
+    "users": { "$ref": "#/definitions/users" },
     "projects": {
       "type": "array",
       "minItems": 5,
@@ -55,81 +126,13 @@ var schema = {
             "type": "string",
             "faker": "date.recent"
           },
-          "tasks": {
-            "type": "array",
-            "minItems": 5,
-            "maxItems": 10,
-            "items": {
-              "type": "object",
-              "properties": {
-                "id": {
-                  "type": "number",
-                  "unique": true,
-                  "minimum": 1
-                },
-                "title": {
-                  "type": "string",
-                  "faker": "hacker.phrase"
-                },
-                "user": {
-                  "type": "object",
-                  "properties": {
-                    "id": {
-                      "type": "number",
-                      "unique": true,
-                      "minimum": 1
-                    },
-                    "firstName": {
-                      "type": "string",
-                      "faker": "name.firstName"
-                    },
-                    "lastName": {
-                      "type": "string",
-                      "faker": "name.lastName"
-                    },
-                    "email": {
-                      "type": "string",
-                      "faker": "internet.email"
-                    }
-                  },
-                  "required": ["id", "firstName", "lastName", "email"]
-                },
-                "createdAt": {
-                  "type": "string",
-                  "faker": "date.recent"
-                },
-                "records": {
-                  "type": "array",
-                  "minItems": 3,
-                  "maxItems": 5,
-                  "items": {
-                    "type": "object",
-                    "properties": {
-                      "id": {
-                        "type": "number",
-                        "unique": true,
-                        "minimum": 1
-                      },
-                      "createdAt": {
-                        "type": "string",
-                        "faker": "date.recent"
-                      },
-                      "finishedAt": {
-                        "type": "string",
-                        "faker": "date.future"
-                      }
-                    },
-                    "required": ["id", "createdAt", "finishedAt"]
-                  }
-                }
-              },
-              "required": ["id", "title", "user", "createdAt", "records"]
-            }
-          }
+          "tasks": { "$ref": "#/definitions/tasks" }
         },
         "required": ["id", "users", "title", "tasks", "createdAt"]
       }
-    }
+    },
+    "tasks": { "$ref": "#/definitions/tasks" },
+    "records": { "$ref": "#/definitions/records" }
   },
   "required": ["project"]
 };
